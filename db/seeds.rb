adde13039782c9a4f91b3a9e6d06c142dac92944
vinicius <viniciusdegouvea2@hotmail.com>
2017-05-29 19:20:28 -0400

    added models of categories and ads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories= [ 
+  "Animais e acessórios",
+  "Esportes",
+  "Para sua casa",
+  "Eletrônicos e celulares",
+  "Música e hobbies",
+  "Bebês e crianças",
+  "Moda e beleza",
+  "Veículos e barcos",
+  "Imóveis",
+  "Empregos e negócios",  
+]
+
+categories.each do |category|
+  Category.find_or_create_by!(description: category)  
+end
